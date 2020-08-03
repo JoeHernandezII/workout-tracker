4bae53684a5ee230e9117d76ab1be1aa2e9ddb5d
Joe Hernandez <yoshi.jhii@gmail.com>
2020-08-03 10:13:24 -0500

    verifying workout code

--- models/Workout.js
+++ models/Workout.js
@@ -3,37 +3,48 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
+    exercises: [
+        {
+            type: {
+                type: String,
+            },
+            name: {
+                type: String
+            },
+            duration: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            }
+        }
+    ],
     day: {
         type: Date,
-        default: Date.now
-    },
-    exercises: [{
-        type: {
-            type: String,
-            trim: true,
-        },
-        name: {
-            type: String,
-            trim: true,
-        },
-        duration: {
-            type: Number,
-        },
-        weight: {
-            type: Number,
-        },
-        reps: {
-            type: Number,
-        },
-        sets: {
-            type: Number,
-        },
-        distance: {
-            type: Number,
-        },
-    }]
+        default: () => new Date()
+    }
+},
+    {
+        toJSON: {
+            virtuals: true
+        }
+    });
+
+WorkoutSchema.virtual("totalDuration").get(function () {
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
 });
 
-const Workout = mongoose.model("Workout", WorkoutSchema);
+const Workout = mongoose.model("workout", WorkoutSchema);
 
 module.exports = Workout;
