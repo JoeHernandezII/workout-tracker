58655b73cf3ca4f89f7d3a6e6c9486ab26de76ce
Joe Hernandez <yoshi.jhii@gmail.com>
2020-08-03 19:37:27 -0500

    updated

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Workout = require("../models/Workout");
+const Workout = require("../models/WorkoutSchema");
 
 
 router.get("/api/workouts", (req, res) => {
